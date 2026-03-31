a480214b5fb19d2e53578e0a7fbc071d56ea969f
g-akca <gok.akca66@gmail.com>
2026-03-31 15:56:48 +0300

    Add PlanSection structure and styles

--- src/components/PlanSection.jsx
+++ src/components/PlanSection.jsx
@@ -1,7 +1,19 @@
+import musicIcon from "/images/icon-music.svg";
+
 function PlanSection() {
   return (
-    <>
-    </>
+    <section className="p-4 bg-blue-50 rounded-[11px] flex justify-between items-center">
+      <div className="flex gap-4 items-center">
+        <img src={musicIcon} alt="Music icon" className="h-12" />
+
+        <div>
+          <p className="font-black text-blue-950">Annual Plan</p>
+          <p className="font-medium">$59.99/year</p>
+        </div>
+      </div>
+
+      <button className="text-blue-700 font-bold cursor-pointer">Change</button>
+    </section>
   )
 }
 
