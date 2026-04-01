fd015d6f65805f27f385014db8f012b3fc40fba7
g-akca <gok.akca66@gmail.com>
2026-04-01 07:58:18 +0300

    Add tablet responsive App and SummaryCard styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import SummaryCard from "./components/SummaryCard";
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-100 bg-[url('/images/pattern-background-mobile.svg')] bg-no-repeat bg-contain bg-top px-6 flex justify-center items-center text-base leading-base text-gray-600">
+    <div className="min-h-screen bg-blue-100 bg-[url('/images/pattern-background-mobile.svg')] bg-no-repeat bg-size-[100%_auto] bg-top px-6 flex justify-center items-center text-base tablet:text-[16px] leading-base text-gray-600">
       <SummaryCard />
     </div>
   )
--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -9,9 +9,12 @@ function SummaryCard() {
     <div className="bg-white rounded-[20px] overflow-hidden grow max-w-112.5">
       <img src={heroImg} alt="Illustration of someone listening to music" />
 
-      <div className="p-6 flex flex-col gap-6">
-        <SummarySection />
-        <PlanSection />
+      <div className="p-6 tablet:p-12 flex flex-col gap-6 tablet:gap-8">
+        <div className="flex flex-col gap-6 tablet:gap-4">
+          <SummarySection />
+          <PlanSection />
+        </div>
+        
         <PaymentSection />
       </div>
     </div>
